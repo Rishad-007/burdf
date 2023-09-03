cff622efeea0eed751523eec90c4d60f2c475365
Rishad-007 <rishad.nur007@gmail.com>
2023-09-03 12:26:16 +0600

    added google form link

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -16,8 +16,14 @@ export const Events = ({ Event_name_1, Time_1,Time_2,Time_3, Event_name_2, Event
             <div class="bg-white p-6 rounded-lg shadow-md">
               <h3 class="text-xl font-semibold mb-4">Practice Schedule</h3>
               <p class="text-gray-700 mb-2">Wednesday: 4:30 PM - 6:00 PM</p>
-              <p class="text-gray-700">Saterday: 4:30 PM - 6:00 PM</p>
+              <p class="text-gray-700">Satarday: 4:30 PM - 6:00 PM</p>
+
+              <div className="py-10">
+                To become new member,{" "}
+                <a href="https://forms.gle/4yDhqeKhBXtjgWPr7" className="text-cyan-600">Fill this LINK</a>
+              </div>
             </div>
+
             {/* <!-- Upcoming Events --> */}
             <div class="bg-white p-6 rounded-lg shadow-md">
               <h3 class="text-xl font-semibold mb-4">Upcoming Events</h3>
@@ -34,7 +40,6 @@ export const Events = ({ Event_name_1, Time_1,Time_2,Time_3, Event_name_2, Event
                     >
                       {/* <!-- Your SVG icon code here --> Date: October 10 */}
                     </svg>
-                    
                   </div>
                   <p class="text-gray-700 font-semibold mb-1">{Event_name_1}</p>
                   <p class="text-gray-500 text-sm mb-2">{Time_1}</p>
