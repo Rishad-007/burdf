b0dbd532c5811b298c5353bca178740535e5703f
Rishad-007 <rishad.nur007@gmail.com>
2023-09-03 13:05:03 +0600

    added new member reg notice.2

--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
-export const Notice = ({tittle,discription,link}) => {
+export const Notice = ({ tittle, discription, link }) => {
   return (
     <div>
       {/* <!-- Notice 1 --> */}
-      <div class="bg-white h-72 p-6 rounded-lg shadow-xl">
+      <div class="bg-white h-100  p-6 rounded-lg shadow-xl">
         <h3 class="text-xl font-semibold mb-2">{tittle}</h3>
         <p class="text-gray-700">{discription}</p>
-        <a href={link} class="mt-4 inline-block text-blue-600 hover:underline">View on Facebook</a>
+        <a href={link} class="mt-4 inline-block text-blue-600 hover:underline">
+          View on Facebook
+        </a>
       </div>
     </div>
-  
-
-      
-  
   );
 };
